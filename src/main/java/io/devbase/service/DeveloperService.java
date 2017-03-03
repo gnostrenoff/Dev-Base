8354d4636c9090abac17549ad5273d4465038d86
gnostrenoff <g.nostrenoff@gmail.com>
2017-03-03 12:00:57 +0100

    [clean] Added come comments

--- src/main/java/io/devbase/service/DeveloperService.java
+++ src/main/java/io/devbase/service/DeveloperService.java
@@ -6,12 +6,37 @@ import java.util.List;
 
 public interface DeveloperService {
 
+  /**
+   * Create/update a developer.
+   *
+   * @param developer the developer
+   * @return the developer created
+   */
   Developer create(Developer developer);
 
+  /**
+   * Set th elanguage for a developper.
+   *
+   * @param languageId  the lang id
+   * @param developerId the dev id
+   * @return the developer updated
+   */
   Developer setLanguage(long languageId, long developerId);
 
+  /**
+   * Get a list of developer for the given lang id.
+   *
+   * @param id the lang id
+   * @return the list of dev
+   */
   List<Developer> getByLanguageId(long id);
 
+  /**
+   * Get a list of developer for the given lang name.
+   *
+   * @param name the lang name
+   * @return the list of dev
+   */
   List<Developer> getByLanguageName(String name);
 
 }
--- src/main/java/io/devbase/service/LanguageService.java
+++ src/main/java/io/devbase/service/LanguageService.java
@@ -4,8 +4,20 @@ import io.devbase.model.Language;
 
 public interface LanguageService {
 
+  /**
+   * get a language by Id.
+   *
+   * @param id the id
+   * @return the language
+   */
   Language getById(long id);
 
+  /**
+   * Create a Language.
+   *
+   * @param language the language to create
+   * @return the language created
+   */
   Language create(Language language);
 
 }
